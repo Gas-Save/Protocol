bfd5214e5138c1045cce9e1e8c53ca1225670919
GSVE Anon <anon@gassave.org>
2021-04-10 11:59:17 +0100

    added gas prints

--- test/SG3Token.js
+++ test/SG3Token.js
@@ -6,17 +6,25 @@ contract("SG3 Token Test", async accounts => {
     const account_one = accounts[0];
     var instance = await SG3Token.new();
     const amountMint = 100;
-    await instance.mint(amountMint, {from: accounts[1]})
+
+    var receipt = await instance.mint(amountMint, {from: accounts[1]})
     const balance = await instance.balanceOf.call(accounts[1]);
     assert.equal(balance.toNumber(), 99);
+    
+    const gasUsed = receipt.receipt.gasUsed;
+    console.log(`GasUsed: ${receipt.receipt.gasUsed}`);
   });
 
   it("should be able to get fee of tokens", async () => {
     const account_one = accounts[0];
     var instance = await SG3Token.new();
     const amountMint = 100;
-    await instance.mint(amountMint, {from: accounts[1]})
+    
+    var receipt = await instance.mint(amountMint, {from: accounts[1]})
+
     const balance = await instance.balanceOf.call(accounts[0]);
     assert.equal(balance.toNumber(), 1);
+    const gasUsed = receipt.receipt.gasUsed;
+    console.log(`GasUsed: ${receipt.receipt.gasUsed}`);
   });
 });
