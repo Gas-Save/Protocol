9c9a1e88b5e879eb0c785e8e9da81b065ec057bb
GSVE Anon <anon@gassave.org>
2021-04-10 12:49:42 +0100

    added helper code
    TODO: Further Tests!

---
+++ contracts/test_helpers/SG3_helper.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.8.0;
+
+import "../ISGToken.sol";
+
+contract SG3_helper {
+
+    function dummy() public {
+        assembly{
+            invalid()
+        }
+    }
+
+    // Burns at least burn gas by calling itself and throwing
+    function burnGas(uint256 burn) public {
+        // call self.dummy() to burn a bunch of gas
+        assembly {
+            mstore(0x0, 0x32e43a1100000000000000000000000000000000000000000000000000000000)
+            let ret := call(burn, address(), 0, 0x0, 0x04, 0x0, 0)
+        }
+    }
+
+    function burnGasAndFree(address gas_token, uint256 burn, uint256 free) public {
+        burnGas(burn);
+        require(ISGToken(gas_token).free(free) > 0, "burnGasAndFree");
+    }
+
+    function burnGasAndFreeUpTo(address gas_token, uint256 burn, uint256 free) public {
+        burnGas(burn);
+        require(free == ISGToken(gas_token).freeUpTo(free), "burnGasAndFreeUpTo");
+    }
+
+    function burnGasAndFreeFrom(address gas_token, uint256 burn, uint256 free) public {
+        burnGas(burn);
+        require(ISGToken(gas_token).freeFrom(tx.origin, free) > 0, "burnGasAndFreeFrom");
+    }
+
+    function burnGasAndFreeFromUpTo(address gas_token, uint256 burn, uint256 free) public {
+        burnGas(burn);
+        require(free == ISGToken(gas_token).freeFromUpTo(tx.origin, free), "burnGasAndFreeFromUpTo");
+    }
+}
--- test/SG3Token.js
+++ test/SG3Token.js
@@ -1,30 +1,76 @@
+const { BN, expectRevert, send, ether } = require('@openzeppelin/test-helpers');
 const SG3Token  = artifacts.require ("./SG3Token.sol");
+const SG3_helper  = artifacts.require ("./test_helpers/SG3_helper.sol");
 
 contract("SG3 Token Test", async accounts => {
+    /*
+    it("should be able to mint tokens", async () => {
+        const account_one = accounts[0];
+        var instance = await SG3Token.new();
+        const amountMint = 100;
 
-  it("should be able to mint tokens", async () => {
-    const account_one = accounts[0];
-    var instance = await SG3Token.new();
-    const amountMint = 100;
+        var receipt = await instance.mint(amountMint, {from: accounts[1]})
+        const balance = await instance.balanceOf.call(accounts[1]);
+        assert.equal(balance.toNumber(), 99);
 
-    var receipt = await instance.mint(amountMint, {from: accounts[1]})
-    const balance = await instance.balanceOf.call(accounts[1]);
-    assert.equal(balance.toNumber(), 99);
-    
-    const gasUsed = receipt.receipt.gasUsed;
-    console.log(`GasUsed: ${receipt.receipt.gasUsed}`);
-  });
+        const gasUsed = receipt.receipt.gasUsed;
+        console.log(`GasUsed: ${receipt.receipt.gasUsed}`);
+    });
 
-  it("should be able to get fee of tokens", async () => {
-    const account_one = accounts[0];
-    var instance = await SG3Token.new();
-    const amountMint = 100;
-    
-    var receipt = await instance.mint(amountMint, {from: accounts[1]})
+    it("should be able to get fee of tokens", async () => {
+        const account_one = accounts[0];
+        var instance = await SG3Token.new();
+        const amountMint = 100;
 
-    const balance = await instance.balanceOf.call(accounts[0]);
-    assert.equal(balance.toNumber(), 1);
-    const gasUsed = receipt.receipt.gasUsed;
-    console.log(`GasUsed: ${receipt.receipt.gasUsed}`);
-  });
+        var receipt = await instance.mint(amountMint, {from: accounts[1]})
+
+        const balance = await instance.balanceOf.call(accounts[0]);
+        assert.equal(balance.toNumber(), 1);
+        const gasUsed = receipt.receipt.gasUsed;
+        console.log(`GasUsed: ${receipt.receipt.gasUsed}`);
+    }); */
+
+    it('Should mint', async function () {
+        var instance = await SG3Token.new();
+        await instance.mint(100, {from:accounts[0]});
+        var total_supply =  await instance.totalSupply.call()
+        total_supply = total_supply.toNumber()
+        assert.equal(total_supply, 100);
+    });
+
+    it('Should fail to free up', async function () {
+        var instance = await SG3Token.new();
+        expectRevert(instance.free(100, {from: accounts[3]}), 'ERC20: burn amount exceeds balance');
+    });
+/*
+    it('Should burnGasAndFreeFrom', async function () {
+        var instance = await SG3Token.new();
+        var helper = await SG3_helper.new();
+
+        await instance.SG3Token.mint(100);
+        await instance.SG3Token.approve(helper.address, 50, {from: accounts[0]});
+        await helper.burnGasAndFreeFrom(instance.address, 5000000, 50, {from:accounts[0]});
+        expect((await instance.totalSupply()).toString()).to.be.equal('50');
+    });
+
+    it('Should burnGasAndFree', async function () {
+        await this.SG3Token.transfer(this.SG3_helper.address, 75);
+        await this.SG3_helper.burnGasAndFree(this.SG3Token.address, 5000000, 75);
+        expect((await this.SG3Token.totalSupply()).toString()).to.be.equal('175');
+    });
+
+    it('Should burnGasAndFreeUpTo', async function () {
+        await this.SG3Token.mint(75);
+        await this.SG3Token.transfer(this.SG3_helper.address, 75);
+        await this.SG3_helper.burnGasAndFreeUpTo(this.SG3Token.address, 5000000, 75);
+        expect((await this.SG3Token.totalSupply()).toString()).to.be.equal('175');
+    });
+
+    it('Should burnGasAndFreeFromUpTo', async function () {
+        await this.SG3Token.mint(100);
+        await this.SG3Token.approve(this.SG3_helper.address, 70);
+        await this.SG3_helper.burnGasAndFreeFromUpTo(this.SG3Token.address, 5000000, 70);
+        expect((await this.SG3Token.totalSupply()).toString()).to.be.equal('205');
+    });
+    */
 });
