f7737b0c6b0dde6b19811625bb4b670104692101
GSVE Anon <anon@gassave.org>
2021-04-12 08:47:10 +0100

    added gas swap wrapper

---
+++ contracts/GasSwapWrapper.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.8.0;
+
+import "./openzepplin/Address.sol";
+import "./openzepplin/IERC20.sol";
+import "./ISGToken.sol";
+
+contract GasSwapWrapper {
+    using Address for address;
+
+    function gasSwapCaller(bytes calldata data, address contractAddress, address gas_token, uint256 tokensToBurn) public payable{
+
+        //approve the token burning
+        IERC20(gas_token).approve(address(this), tokensToBurn);
+
+        //check that the address we have is actually a contract that we can call
+        if(!contractAddress.isContract()){
+            return;
+        }
+
+        if(msg.value > 0){
+            contractAddress.functionCallWithValue(data, msg.value, "GS: Error forwarding transaction");
+        }
+        else{
+            contractAddress.functionCall(data, "GS: Error forwarding transaction");
+        }
+
+        ISGToken(gas_token).freeFromUpTo(msg.sender, tokensToBurn);
+    }
+
+    function proxyCaller(bytes memory data, address contractAddress) public payable{
+
+        //check that the address we have is actually a contract that we can call
+        if(!contractAddress.isContract()){
+            return;
+        }
+
+        if(msg.value > 0){
+            contractAddress.functionCallWithValue(data, msg.value, "GS: Error forwarding transaction");
+        }
+        else{
+            contractAddress.functionCall(data, "GS: Error forwarding transaction");
+        }
+    }
+    
+    event Interaction(address indexed owner, bytes data);
+    
+}
