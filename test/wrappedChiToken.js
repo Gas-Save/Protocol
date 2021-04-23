1a14fae33a34fc6ad6e755bb9ebf2de38d826f70
GSVE Anon <anon@gassave.org>
2021-04-23 15:10:22 +0100

    Update wrappedChiToken.js

--- test/wrappedChiToken.js
+++ test/wrappedChiToken.js
@@ -3,12 +3,17 @@ const SG_Helper  = artifacts.require ("./test_helpers/SG_Helper.sol");
 const wrappedChiToken  = artifacts.require ("./wrappedChiToken.sol");
 const ChiToken  = artifacts.require ("./existing_gas_tokens/ChiToken.sol");
 
+var instance;
+var chiTokenInstance;
+
 contract("Wrapped ChiToken Token Test", async accounts => {
  
-    it("should be able to mint & wrap from contracts", async () => {
-        var chiTokenInstance = await ChiToken.new();
-        var instance = await wrappedChiToken.new(chiTokenInstance.address);    
+    it("should deploy", async () => {
+        chiTokenInstance = await ChiToken.new();
+        instance = await wrappedChiToken.new(chiTokenInstance.address);    
+      });
 
+    it("should be able to mint & wrap from contracts", async () => {
         const amountMint = 100;
 
         await chiTokenInstance.mint(amountMint);
