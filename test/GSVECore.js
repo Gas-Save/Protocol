d149976113443badb82248d23016ea2de15de2ce
GSVE Anon <anon@gassave.org>
2021-04-24 20:16:29 +0100

    Create GSVECore.js

---
+++ test/GSVECore.js
@@ -0,0 +1,60 @@
+const { BN, expectRevert, send, ether } = require('@openzeppelin/test-helpers');
+const { web3 } = require('@openzeppelin/test-helpers/src/setup');
+const GSVEToken  = artifacts.require ("./GSVEToken.sol");
+const GSVEProtocol = artifacts.require ("./GSVECore.sol");
+const GasToken  = artifacts.require ("./JetFuel.sol");
+const GS_Wrapper  = artifacts.require ("./GasSwapWrapper.sol");
+const GSVE_helper  = artifacts.require ("./test_helpers/GSVE_helper.sol");
+
+contract("GSVE Token Test", async accounts => {
+    var token;
+    var gasToken;
+    var protocol;
+    const TOTAL_SUPPLY = web3.utils.toWei('100000000');
+
+    it('should be able to deploy protocol contracts', async () => {
+      token = await GSVEToken.new();
+      gasToken = await GasToken.new();
+      protocol = await GSVEProtocol.new(token.address);
+    });
+
+    it("should be able to update the fee address of the gas token to the protocol", async () => {
+      await gasToken.updateFeeAddress(protocol.address);
+      var owner = await gasToken.feeAddress()
+      assert.equal(owner, protocol.address);
+    });
+
+    it("should be able to transfer ownership of a gas token to the protocol", async () => {
+      await gasToken.transferOwnership(protocol.address);
+      var owner = await gasToken.owner()
+      assert.equal(owner, protocol.address);
+    });
+
+    it('should be able to add a token to the list of supported tokens', async () => {
+
+      await protocol.addGasToken(gasToken.address, 1, 1);
+
+      var claimable = await protocol.claimable(gasToken.address);
+      var mintType = await protocol.mintingType(gasToken.address);
+      assert.equal(claimable.toNumber(), 1);
+      assert.equal(mintType.toNumber(), 1);
+    });
+  
+    it('should be able to burn gsve to save on protocol minting fee', async () => {
+      await token.approve(protocol.address, web3.utils.toWei("10"));
+      var receipt = await protocol.burnDiscountedMinting(gasToken.address, 100);
+
+      const gasTokenBalance = await gasToken.balanceOf.call(accounts[0]);
+      assert.equal(gasTokenBalance, 100);
+
+      const feeHolderGasTokenBalance = await gasToken.balanceOf.call(protocol.address);
+      assert.equal(feeHolderGasTokenBalance, 0);
+
+      const New_SUPPLY = web3.utils.toWei('99999999.75');
+      const totalSupplyGSVE = await token.totalSupply();
+      const balanceAdmin = await token.balanceOf(accounts[0]);
+      assert.equal(totalSupplyGSVE.toString(), New_SUPPLY);
+      assert.equal(balanceAdmin.toString(), New_SUPPLY);
+    });
+
+});
