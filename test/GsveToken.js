ad36f43b4ca5848001d781cfe831516488b1a9b9
GSVE Anon <anon@gassave.org>
2021-04-24 19:08:31 +0100

    Update GsveToken.js

--- test/GsveToken.js
+++ test/GsveToken.js
@@ -23,4 +23,32 @@ contract("GSVE Token Test", async accounts => {
       assert.equal(name.toString(), "Gas Save Utility Token by Gas Save");
       assert.equal(symbol.toString(), "GSVE");
   });
+
+  it("should be able to burn tokens", async () => {
+
+    await token.burn(web3.utils.toWei("1"));
+    const New_SUPPLY = web3.utils.toWei('99999999');
+    const totalSupply = await token.totalSupply();
+    const balanceAdmin = await token.balanceOf(accounts[0]);
+    assert.equal(totalSupply.toString(), New_SUPPLY);
+    assert.equal(balanceAdmin.toString(), New_SUPPLY);
+  });
+
+  it("should not be able to burn tokens if has no tokens", async () => {
+    expectRevert(token.burn(web3.utils.toWei("1"), {from:accounts[1]}), 'ERC20: burn amount exceeds balance');
+  });
+
+  it("should be able to burn tokens for another account if approved", async () => {
+    await token.approve(accounts[1], web3.utils.toWei("1"))
+    await token.burnFrom(accounts[0], web3.utils.toWei("1"), {from:accounts[1]});
+    const New_SUPPLY = web3.utils.toWei('99999998');
+    const totalSupply = await token.totalSupply();
+    const balanceAdmin = await token.balanceOf(accounts[0]);
+    assert.equal(totalSupply.toString(), New_SUPPLY);
+    assert.equal(balanceAdmin.toString(), New_SUPPLY);
+  });
+
+  it("should not be able to burn tokens when not approved", async () => {
+    expectRevert(token.burnFrom(accounts[0], web3.utils.toWei("1"), {from: accounts[1]}), 'ERC20: burn amount exceeds allowance.');
+  });
 });
