1152715c9a9257d869b158f3bf713b9693a96cbe
GSVE Anon <anon@gassave.org>
2021-04-28 20:31:31 +0100

    finishedContract

--- contracts/GSVEContractDeployer.sol
+++ contracts/GSVEContractDeployer.sol
@@ -8,6 +8,7 @@ interface IFreeFromUpTo {
 
 contract GSVEContractDeployer is Ownable{
     mapping(address => uint256) private _compatibleGasTokens;
+    uint256 private _salt = 0;
 
     function addGasToken(address gasToken) public onlyOwner{
         _compatibleGasTokens[gasToken] = 1;
@@ -17,7 +18,11 @@ contract GSVEContractDeployer is Ownable{
         return _compatibleGasTokens[gasToken];
     }
 
-    modifier DiscountGas(address gasToken) {
+    function salt() public view returns(uint256){
+        return _salt;
+    }
+
+    modifier discountGas(address gasToken) {
         require(_compatibleGasTokens[gasToken] == 1, "GSVE: incompatible token");
         uint256 gasStart = gasleft();
         _;
@@ -25,19 +30,32 @@ contract GSVEContractDeployer is Ownable{
         IFreeFromUpTo(gasToken).freeFromUpTo(msg.sender,  (gasSpent + 14154) / 41130);
     }
 
-    function GsveDeploy(bytes memory data, address gasToken) public DiscountGas(gasToken) returns(address contractAddress) {
+    function GsveDeploy(bytes memory data, address gasToken) public discountGas(gasToken) returns(address contractAddress) {
         assembly {
             contractAddress := create(0, add(data, 32), mload(data))
         }
-        emit ContractDeployed(contractAddress);
+        try Ownable(contractAddress).transferOwnership(msg.sender){
+            emit ContractDeployed(contractAddress);
+        }
+        catch{
+            emit ContractDeployed(contractAddress);
+        }
     }
 
-    function GsveDeploy2(uint256 salt, bytes memory data, address gasToken) public DiscountGas(gasToken) returns(address contractAddress) {
+    function GsveDeploy2(bytes memory data, address gasToken) public discountGas(gasToken) returns(address contractAddress) {
+        uint256 currentSalt = _salt;
         assembly {
-            contractAddress := create2(0, add(data, 32), mload(data), salt)
+            contractAddress := create2(0, add(data, 32), mload(data), currentSalt)
         }
 
-        emit ContractDeployed(contractAddress);
+        _salt = _salt + 1;
+
+        try Ownable(contractAddress).transferOwnership(msg.sender){
+            emit ContractDeployed(contractAddress);
+        }
+        catch{
+            emit ContractDeployed(contractAddress);
+        }
     }
 
     event ContractDeployed(address indexed deploymentAddress);
--- test/GSVEContractDeployer.js
+++ test/GSVEContractDeployer.js
@@ -2,8 +2,8 @@ const { BN, expectRevert, send, ether } = require('@openzeppelin/test-helpers');
 const { web3 } = require('@openzeppelin/test-helpers/src/setup');
 const GasToken  = artifacts.require ("./JetFuel.sol");
 const GS_Deployer  = artifacts.require ("./GSVEContractDeployer.sol");
-const timeMachine = require('ganache-time-traveler');
-const byteCodeJson = require("build/contracts/GasSwapWrapper.json")
+const byteCodeJson = require("./../build/contracts/GasSwapWrapper.json")
+const byteCodeNonOwnedJson = require("./../build/contracts/GasToken1.json")
 
 contract("GSVE Contract Deployer Test", async accounts => {
     var gasToken;
@@ -20,85 +20,53 @@ contract("GSVE Contract Deployer Test", async accounts => {
     it('should be able to add a token to the list of supported tokens', async () => {
       await deployer.addGasToken(gasToken.address);
 
-      var compatible = await protocol.compatibleGasToken(gasToken.address);
+      var compatible = await deployer.compatibleGasToken(gasToken.address);
       assert.equal(compatible.toNumber(), 1);
     });
   
 
     it('should revert when trying to use a non-supported gas token address', async () => {
-      var byteCode = byteCodeJson["bytecode"]
+      var byteCode = byteCodeJson["bytecode"];
       expectRevert(deployer.GsveDeploy(byteCode, deployer.address, {from: accounts[1]}), "GSVE: incompatible token");
     });
 
-    /*
-    it('should fail to discount minting as no tokens to burn but approval given', async () => {
-      await token.approve(protocol.address, web3.utils.toWei("0.25"), {from: accounts[1]});
-      expectRevert(protocol.burnDiscountedMinting(gasToken.address, 100, {from: accounts[1]}), 'ERC20: burn amount exceeds balance.');
+    it('should be able to deploy a contract even when no gas tokens on hand', async () => {
+      var byteCode = byteCodeJson["bytecode"];
+      var receipt = await deployer.GsveDeploy(byteCode, gasToken.address, {from: accounts[0]});
+      console.log(`GasUsed: ${receipt.receipt.gasUsed}`);
     });
 
-    it('should be able to stake gsve', async () => {
-      await token.approve(protocol.address, web3.utils.toWei("25000"));
-      var receipt = await protocol.stake(web3.utils.toWei('25000'));
-
-      const balanceSent = web3.utils.toWei('25000');
-      const balanceAddress = await token.balanceOf(protocol.address);
-      const totalStaked = await protocol.totalStaked();
-      const userStakeSize = await protocol.userStakeSize(accounts[0]);
-
-      assert.equal(balanceAddress, balanceSent);
-      assert.equal(totalStaked, balanceSent);
-      assert.equal(userStakeSize, balanceSent);
+    it('should be able to deploy a contract that is not a ownable', async () => {
+      var byteCode = byteCodeNonOwnedJson["bytecode"];
+      var receipt = await deployer.GsveDeploy(byteCode, gasToken.address, {from: accounts[0]});
+      console.log(`GasUsed: ${receipt.receipt.gasUsed}`);
     });
 
-    it('should be able to mint at a reduced fee due to already having a tier 1 stake', async () => {
-
-      var receipt = await protocol.discountedMinting(gasToken.address, 100);
-
-      const gasTokenBalance = await gasToken.balanceOf.call(accounts[0]);
-      assert.equal(gasTokenBalance.toNumber(), 199);
-
-      const feeHolderGasTokenBalance = await gasToken.balanceOf.call(protocol.address);
-      assert.equal(feeHolderGasTokenBalance.toNumber(), 1);
+    it('should be able to deploy a contract even when we have gas tokens', async () => {
+      await gasToken.mint(100);
+      await gasToken.approve(deployer.address, 98);
+      var byteCode = byteCodeJson["bytecode"];
+      var receipt = await deployer.GsveDeploy(byteCode, gasToken.address, {from: accounts[0]});
+      console.log(`GasUsed: ${receipt.receipt.gasUsed}`);
     });
 
-    it('should fail to be rewarded if the contract has no rewards to give.', async () => {
-      expectRevert(protocol.rewardedMinting(gasToken.address, 100, {from: accounts[3]}), "GSVE: contract has ran out of rewards to give.");
+    it('should be able to deploy2 a contract even when no gas tokens on hand', async () => {
+      var byteCode = byteCodeJson["bytecode"];
+      var receipt = await deployer.GsveDeploy2(byteCode, gasToken.address, {from: accounts[1]});
+      console.log(`GasUsed: ${receipt.receipt.gasUsed}`);
     });
 
-    it('contract should be given some tokens to distribute', async () => {
-      await token.transfer(protocol.address, web3.utils.toWei("10000000"));
-      const totalRewards = await protocol.totalRewards();
-      assert.equal(totalRewards.toString(), web3.utils.toWei("10000000"));
+    it('should be able to deploy2 a contract that is not ownable', async () => {
+      var byteCode = byteCodeNonOwnedJson["bytecode"];
+      var receipt = await deployer.GsveDeploy2(byteCode, gasToken.address, {from: accounts[1]});
+      console.log(`GasUsed: ${receipt.receipt.gasUsed}`);
     });
 
-    it('should fail to mint at discount rate as not a tier 1 stake', async () => {
-      expectRevert(protocol.discountedMinting(gasToken.address, 100, {from: accounts[1]}), 'GSVE: User has not staked enough to discount.');
+    it('should be able to deploy2 a contract that is not ownable and save gas', async () => {
+      await gasToken.mint(100, {from: accounts[1]});
+      await gasToken.approve(deployer.address, 98, {from: accounts[1]});
+      var byteCode = byteCodeNonOwnedJson["bytecode"]
+      var receipt = await deployer.GsveDeploy2(byteCode, gasToken.address, {from: accounts[1]});
+      console.log(`GasUsed: ${receipt.receipt.gasUsed}`);
     });
-
-    it('should fail to be rewarded for attempting to mint a non-accepted address', async () => {
-      expectRevert(protocol.rewardedMinting(token.address, 100, {from: accounts[3]}), "GSVE: Unsupported Token");
-    });
-
-
-
-    it('should be able to mint tokens and be rewarded with gsve tokens', async () => {
-      var receipt = await protocol.rewardedMinting(gasToken.address, 100, {from: accounts[3]});
-
-      const gasTokenBalance = await gasToken.balanceOf(accounts[3]);
-      assert.equal(gasTokenBalance.toNumber(), 98);
-
-      const gasTokenProtocolBalance = await gasToken.balanceOf(protocol.address);
-      assert.equal(gasTokenProtocolBalance.toNumber(), 3);
-
-      const gsveReward = web3.utils.toWei('0.1');
-      const gsveBalance = await token.balanceOf(accounts[3]);
-      assert.equal(gsveReward.toString(), gsveBalance.toString());
-    });
-
-    it('should be able to claim tokens after x time', async () => {
-      await timeMachine.advanceTimeAndBlock(60 * 60 * 13);
-      
-    });
-    */
-
 });
