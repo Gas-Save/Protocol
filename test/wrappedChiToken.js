adac4b55942523f3381dfa9287f5ffd3842183d3
GSVE Anon <anon@gassave.org>
2021-05-06 07:34:06 +0100

    Update wrappedChiToken.js

--- test/wrappedChiToken.js
+++ test/wrappedChiToken.js
@@ -1,16 +1,19 @@
 const { BN, expectRevert, send, ether } = require('@openzeppelin/test-helpers');
 const GSVE_helper  = artifacts.require ("./test_helpers/GSVE_helper.sol");
-const wrappedToken  = artifacts.require ("./wrappedNonGsveToken.sol");
+const wrappedToken  = artifacts.require ("./WrappedGasToken.sol");
 const GasToken  = artifacts.require ("./existing_gas_tokens/ChiToken.sol");
 
 var instance;
-var chiTokenInstance;
+var baseGasToken;
+var helper;
+const amountMint = 100;
 
 contract("Wrapped ChiToken Token Test", async accounts => {
  
-    it("should be able to mint & wrap from contracts", async () => {
-        chiTokenInstance = await GasToken.new();
-        instance = await wrappedToken.new(chiTokenInstance.address, "Wrapped Chi by Gas Save", "wChi");    
+    it("should be able deploy contracts", async () => {
+        baseGasToken = await GasToken.new();
+        instance = await wrappedToken.new(baseGasToken.address, "Wrapped Chi by Gas Save", "wChi");    
+        helper = await GSVE_helper.new();
     });
 
     it("name and symbol should be correct", async () => {
@@ -20,100 +23,74 @@ contract("Wrapped ChiToken Token Test", async accounts => {
         assert.equal(name.toString(), "Wrapped Chi by Gas Save");
         assert.equal(symbol.toString(), "wChi");
     });
-/*
-    it("should be able to mint & wrap from contracts", async () => {
-        const amountMint = 100;
 
-        await chiTokenInstance.mint(amountMint);
-        await chiTokenInstance.approve(instance.address, amountMint);
-        var receipt = await instance.mint(amountMint)
+    it("should be able to mint & wrap from contracts. Fee and balances should be correct", async () => {
+        await baseGasToken.mint(amountMint, {from: accounts[1]});
+        await baseGasToken.approve(instance.address, amountMint, {from: accounts[1]});
+        var receipt = await instance.mint(amountMint, {from: accounts[1]})
 
         var balance = await instance.balanceOf.call(accounts[0]);
-        assert.equal(balance.toNumber(), 100);
-
-        var balance = await chiTokenInstance.balanceOf.call(instance.address);
-        assert.equal(balance.toNumber(), 100);
-
-        console.log(`GasUsed: ${receipt.receipt.gasUsed}`);
-    });
-
-*/
-
-/*
-    it("should be able to mint tokens", async () => {
+        assert.equal(balance.toNumber(), 3);
         
-        const account_one = accounts[0];
-        var instance = await SG2Token.new();
-        const amountMint = 100;
+        balance = await instance.balanceOf.call(accounts[1]);
+        assert.equal(balance.toNumber(), 97);
 
-        var receipt = await instance.mint(amountMint, {from: accounts[1]})
-        const balance = await instance.balanceOf.call(accounts[1]);
-        assert.equal(balance.toNumber(), 99);
-
-        console.log(`GasUsed: ${receipt.receipt.gasUsed}`);
-    });
-
-    it("should be able to get fee of tokens", async () => {
-        const account_one = accounts[0];
-        var instance = await SG2Token.new();
-        const amountMint = 100;
-
-        var receipt = await instance.mint(amountMint, {from: accounts[1]})
-
-        const balance = await instance.balanceOf.call(accounts[0]);
-        assert.equal(balance.toNumber(), 1);
-
-        console.log(`GasUsed: ${receipt.receipt.gasUsed}`);
-    }); 
-    it('Should mint', async function () {
-        var instance = await SG2Token.new();
-        var receipt = await instance.mint(100, {from:accounts[0]});
-
-        var total_supply =  await instance.totalSupply.call()
-        total_supply = total_supply.toNumber()
-        assert.equal(total_supply, 100);
+        balance = await baseGasToken.balanceOf.call(instance.address);
+        assert.equal(balance.toNumber(), 100);
 
         console.log(`GasUsed: ${receipt.receipt.gasUsed}`);
     });
 
     it('Should fail to free up', async function () {
-        var instance = await SG2Token.new();
-        expectRevert(instance.free(100, {from: accounts[3]}), 'ERC20: burn amount exceeds balance');
+        expectRevert(instance.free(amountMint, {from: accounts[3]}), 'ERC20: burn amount exceeds balance');
     });
 
     it('burn gas to find baseline cost', async function () {
-        var instance = await SG2Token.new();
-        var helper = await GSVE_helper.new();
-
-        await instance.mint(100);
-        await instance.approve(helper.address, 50, {from: accounts[0]});
-        var receipt = await helper.burnGas(5000000, {from: accounts[0]});
-
-        var total_supply =  await instance.totalSupply.call()
-        total_supply = total_supply.toNumber()
+        var receipt = await helper.burnGas(1000000);
         assert.equal(true, true);
-
         console.log(`GasUsed: ${receipt.receipt.gasUsed}`);
     });
-
 
     it('Should burn gas and free from', async function () {
-        var instance = await SG2Token.new();
-        var helper = await GSVE_helper.new();
+        await baseGasToken.mint(amountMint);
+        await baseGasToken.approve(instance.address, amountMint);
 
-        await instance.mint(100);
-        await instance.approve(helper.address, 50, {from: accounts[0]});
-        var receipt = await helper.burnGasAndFreeFrom(instance.address, 5000000, 50, {from: accounts[0]});
+        await instance.mint(amountMint);
+        await instance.approve(helper.address, 50);
+
+        var receipt = await helper.burnGasAndFreeFrom(instance.address, 1000000, 50);
 
         var total_supply =  await instance.totalSupply.call()
-        total_supply = total_supply.toNumber()
-        assert.equal(total_supply, 50;
+        assert.equal(total_supply.toNumber(), 150);
+
+        var balance = await instance.balanceOf.call(accounts[0]);
+        assert.equal(balance.toNumber(), 53);
+
+        balance = await baseGasToken.balanceOf.call(instance.address);
+        assert.equal(balance.toNumber(), 150);
 
         console.log(`GasUsed: ${receipt.receipt.gasUsed}`);
     });
 
+    it('Should burn Gas And Free From Up To', async function () {
+        await instance.approve(helper.address, 53);
+        var receipt = await helper.burnGasAndFreeFromUpTo(instance.address, 1000000, 53);
+        
+        var total_supply =  await instance.totalSupply.call()
+        assert.equal(total_supply.toNumber(), 97);
+
+        var balance = await instance.balanceOf.call(accounts[0]);
+        assert.equal(balance.toNumber(), 0);
+
+        balance = await baseGasToken.balanceOf.call(instance.address);
+        assert.equal(balance.toNumber(), 97);
+
+        console.log(`GasUsed: ${receipt.receipt.gasUsed}`);
+    });
+
+/*
     it('Should burn Gas And Free', async function () {
-        var instance = await SG2Token.new();
+        var instance = await GasToken.new();
         var helper = await GSVE_helper.new();
 
         await instance.mint(100);
@@ -128,7 +105,7 @@ contract("Wrapped ChiToken Token Test", async accounts => {
     });
 
     it('Should burn Gas And Free Up To', async function () {
-        var instance = await SG2Token.new();
+        var instance = await GasToken.new();
         var helper = await GSVE_helper.new();
 
         await instance.mint(100);
@@ -141,20 +118,5 @@ contract("Wrapped ChiToken Token Test", async accounts => {
         
         console.log(`GasUsed: ${receipt.receipt.gasUsed}`);
     });
-
-    it('Should burn Gas And Free From Up To', async function () {
-        var instance = await SG2Token.new();
-        var helper = await GSVE_helper.new();
-
-        await instance.mint(100);
-        await instance.approve(helper.address, 75, {from: accounts[0]});
-        var receipt = await helper.burnGasAndFreeFromUpTo(instance.address, 5000000, 70, {from: accounts[0]});
-        
-        var total_supply =  await instance.totalSupply.call()
-        total_supply = total_supply.toNumber()
-        assert.equal(total_supply, 30); 
-        
-        console.log(`GasUsed: ${receipt.receipt.gasUsed}`);
-    });
-        */
+*/
 });
