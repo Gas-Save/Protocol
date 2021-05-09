447d9b739211ed316e08db707ff4c8116b7683ac
GSVE Anon <anon@gassave.org>
2021-05-09 14:41:13 +0100

    Update 1_initial_migration.js

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -20,21 +20,26 @@ module.exports = async(deployer) => {
 
   console.log("Setting up contracts")
   var vaultInstance = await GSVEVault.at(GSVEVault.address);
+  console.log("vault: " + GSVEVault.address)
   await vaultInstance.transferOwnership(GSVEProtocolCore.address)
 
   var wChiInstance = await wrappedToken.at(wchi.address)
+  console.log("wchi: " + wchi.address)
   await wChiInstance.updateFeeAddress(GSVEVault.address)
   await wChiInstance.transferOwnership(GSVEProtocolCore.address)
 
   var wgst2Instance = await wrappedToken.at(wgst2.address)
+  console.log("wgst2: " + wgst2.address)
   await wgst2Instance.updateFeeAddress(GSVEVault.address)
   await wgst2Instance.transferOwnership(GSVEProtocolCore.address)
 
   var wgst1Instance = await wrappedToken.at(wgst1.address)
+  console.log("wgst1: " + wgst1.address)
   await wgst1Instance.updateFeeAddress(GSVEVault.address)
   await wgst1Instance.transferOwnership(GSVEProtocolCore.address)
 
   var coreInstance = await GSVEProtocolCore.at(GSVEProtocolCore.address)
+  console.log("core: " + GSVEProtocolCore.address)
   await coreInstance.addGasToken("0x0000000000004946c0e9F43F4Dee607b0eF1fA1c", 1, true); // chi
   await coreInstance.addGasToken("0x0000000000b3F879cb30FE243b4Dfee438691c04", 1, true); // gst2
   await coreInstance.addGasToken("0x88d60255F917e3eb94eaE199d827DAd837fac4cB", 1, true); //gst1
@@ -43,18 +48,21 @@ module.exports = async(deployer) => {
   await coreInstance.addGasToken(wgst1.address, 2, true); //wgst1
 
   var wrapperFactoryInstance = await GS_WrapperFactory.at(wrapperFactory.address)
+  console.log("factory: " + wrapperFactory.address)
   await wrapperFactoryInstance.addGasToken(wchi.address, 24000); //wchi
   await wrapperFactoryInstance.addGasToken(wgst2.address, 24000); //wgst2
   await wrapperFactoryInstance.addGasToken(wgst1.address, 15000); //wgst1
   await wrapperFactoryInstance.transferOwnership(GSVEProtocolCore.address)
 
   var deployercontractInstance = await GS_Deployer.at(deployercontract.address)
+  console.log("deployer: " + deployercontract.address)
   await deployercontractInstance.addGasToken(wchi.address, 41130); //wchi
   await deployercontractInstance.addGasToken(wgst2.address, 41130); //wgst2
   await deployercontractInstance.addGasToken(wgst1.address, 25130); //wgst1
   await deployercontractInstance.transferOwnership(GSVEProtocolCore.address)
 
   var wrapperMainInstance = await GS_WrapperFactory.at(GS_Wrapper.address)
+  console.log("wrapper: " + GS_Wrapper.address)
   await wrapperMainInstance.transferOwnership(GSVEProtocolCore.address)
   
 };
